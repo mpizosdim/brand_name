a28e027f025470c85a2b4a73d5ac241a3f2a1bfe
mpizosdim <dimitrisbizopoulos@gmail.com>
2018-09-21 12:48:37 +0200

    save functionality

--- main.py
+++ main.py
@@ -8,33 +8,33 @@ from keras.utils import to_categorical
 from keras.callbacks import Callback
 from keras.callbacks import ModelCheckpoint
 import re
+import pickle
+from keras.models import load_model as load_model_keras
 
 
 class GenerateNames(Callback):
-    def __init__(self, char_to_int, int_to_char, num_names, max_size, vocab_size):
-        self.char_to_int_dic = char_to_int
-        self.int_to_char_dic = int_to_char
+    def __init__(self, lstm_model, num_names):
+        self.lstm_model = lstm_model
         self.num_names = num_names
-        self.max_size = max_size
-        self.vocab_size = vocab_size
 
     def on_epoch_end(self, epoch, logs={}):
         for i in range(0, self.num_names):
             stop = False
-            index = np.random.randint(1, self.vocab_size)
-            ch = self.int_to_char_dic[index]
+            index = np.random.randint(1, self.lstm_model.vocab_size)
+            ch = self.lstm_model.int2char_dict[index]
             counter = 1
-            target_seq = np.zeros((1, self.max_size, self.vocab_size))
-            target_seq[0, 0, self.char_to_int_dic[ch]] = 1.
-            while stop == False and counter < self.max_size:
-                probs = self.model.predict(target_seq, verbose=0)[:, counter-1, :]
-                index = np.random.choice(range(self.vocab_size), p=probs.ravel())
-                c = self.int_to_char_dic[index]
+            target_seq = np.zeros((1, self.lstm_model.longest_sentence, self.lstm_model.vocab_size))
+            target_seq[0, 0, self.lstm_model.char2int_dict[ch]] = 1.
+            while stop == False and counter < self.lstm_model.longest_sentence:
+                probs = self.lstm_model.model.predict(target_seq, verbose=0)[:, counter - 1, :]
+                #probs = self.model.predict(target_seq, verbose=0)[:, counter - 1, :] NOT SURE WHICH IS CORRECT
+                index = np.random.choice(range(self.lstm_model.vocab_size), p=probs.ravel())
+                c = self.lstm_model.int2char_dict[index]
                 if c == '\n':
                     stop = True
                 else:
                     ch = ch + c
-                    target_seq[0, counter, self.char_to_int_dic[c]] = 1.
+                    target_seq[0, counter, self.lstm_model.char2int_dict[c]] = 1.
                     counter = counter + 1
 
             print(ch)
@@ -56,6 +56,22 @@ class NamerLstm(object):
         self.stopwords = stopwords
         pass
 
+    def save_info(self):
+        additional_info = {"vocab_size": self.vocab_size, "longest_sentence": self.longest_sentence}
+        with open('info.pkl', 'wb') as f:
+            pickle.dump([self.char2int_dict, self.int2char_dict, additional_info], f, pickle.HIGHEST_PROTOCOL)
+
+    def load_model(self, path):
+        self.model = load_model_keras(path)
+
+    def load_info(self, path):
+        with open(path, 'rb') as f:
+            info = pickle.load(f)
+            self.char2int_dict = info[0]
+            self.int2char_dict = info[1]
+            self.vocab_size = info[2]["vocab_size"]
+            self.longest_sentence = info[2]["longest_sentence"]
+
     def __pad_data(self, X, Y, longest_sentence):
         X = sequence.pad_sequences(X, maxlen=longest_sentence, padding="post")
         Y = sequence.pad_sequences(Y, maxlen=longest_sentence, padding="post")
@@ -69,7 +85,7 @@ class NamerLstm(object):
         examples = data.split("\n")
         examples = [re.sub(' +', ' ', x).strip() for x in examples]
         examples = [x for x in examples if self.__is_english(x)] if only_english else examples
-        examples = [self.__remove_stopwords(x) for x in examples] if stopwords else examples
+        examples = [self.__remove_stopwords(x) for x in examples] if self.stopwords else examples
         examples = [x for x in examples if len(x) >= min_len]
         return examples
 
@@ -125,28 +141,64 @@ class NamerLstm(object):
         dense_layer = TimeDistributed(Dense(self.vocab_size))(lstm_layer)
         actication_layer = TimeDistributed(Activation('softmax'))(dense_layer)
         model = Model(inputs=[input_layer], outputs=[actication_layer])
-        self.hidden_size = hidden_size
         self.model = model
 
-    def fit(self, lr=0.01, epoch_size=2, names_to_output=10):
+    def fit(self, lr=0.01, epoch_size=100, names_to_output_per_epoch=10):
         rmsp = RMSprop(lr=lr)
         self.model.compile(optimizer=rmsp, loss='categorical_crossentropy', metrics=["categorical_accuracy"])
-        gen = GenerateNames(self.char2int_dict, self.int2char_dict, names_to_output, self.longest_sentence, self.vocab_size)
+        gen = GenerateNames(self, names_to_output_per_epoch)
         checkpoint = ModelCheckpoint("model_%s_%s" % (self.hidden_size, lr), monitor='loss', verbose=1, save_best_only=True, mode='min')
         self.model.fit(x=self.X, y=self.Y, epochs=epoch_size, callbacks=[gen, checkpoint])
+        self.save_info()
+
+    def get_names(self, num_of_names):
+        for i in range(0, num_of_names):
+            stop = False
+            index = np.random.randint(1, self.vocab_size)
+            ch = self.int2char_dict[index]
+            counter = 1
+            target_seq = np.zeros((1, self.longest_sentence, self.vocab_size))
+            target_seq[0, 0, self.char2int_dict[ch]] = 1.
+            while stop == False and counter < self.longest_sentence:
+                probs = self.model.predict(target_seq, verbose=0)[:, counter - 1, :]
+                index = np.random.choice(range(self.vocab_size), p=probs.ravel())
+                c = self.int2char_dict[index]
+                if c == '\n':
+                    stop = True
+                else:
+                    ch = ch + c
+                    target_seq[0, counter, self.char2int_dict[c]] = 1.
+                    counter = counter + 1
+
+            print(ch)
 
     def summary(self):
         print('vocab size is: %d' % self.vocab_size)
         print('size of data is: %s' % self.X.shape)
 
 
-if __name__ == '__main__':
+# ====== mains ==========
+
+
+def main_train():
     hidden_size = 250
-    number_of_names = 10
     stopwords = ['shoes', 'paris', 'london', 'milano', 'jeans', 'eyewear', 'jewelry']
 
     namerAlgo = NamerLstm(hidden_size)
     namerAlgo.set_stopwords(stopwords)
     namerAlgo.create_dataset("brandnames.csv", lowercase=True, additional_clean=True, min_len=2, only_english=True)
     namerAlgo.build_model()
-    namerAlgo.fit()
+    namerAlgo.fit(epoch_size=10)
+    namerAlgo.get_names(2)
+
+def main_load():
+    hidden_size = 250
+    namerAlgo = NamerLstm(hidden_size)
+    namerAlgo.load_info("info.pkl")
+    namerAlgo.build_model()
+    namerAlgo.load_model("model_250_0.01")
+    namerAlgo.get_names(10)
+
+if __name__ == '__main__':
+    main_load()
+
